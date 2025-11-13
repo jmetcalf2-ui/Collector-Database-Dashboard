f636bfdd804cb8e4dcadff1fa76a10b2c04a058a
jmetcalf2-ui <jmetcalf2@fordham.edu>
2025-11-13 09:46:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -320,7 +320,7 @@ with tabs[1]:
             country_val = lead.get("country") or ""
 
             # CLICKABLE ROW
-            name_to_tier = "&nbsp;" * 74
+            name_to_tier = "&nbsp;" * 75
             tier_to_email = "&nbsp;" * 14
             
             label = (
